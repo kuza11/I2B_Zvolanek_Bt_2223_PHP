70efae1be42801531d8aa0115f0056f47b7671b5
zvolaja <zvolaja@student.uzlabina.cz>
2023-02-13 22:02:45 +0100

    ASCII done

--- ASCII_art/index.php
+++ ASCII_art/index.php
@@ -28,19 +28,19 @@
         $hw = $_GET['hw'];
         $hh = $_GET['hh'];
         $fh = $_GET['fh'];
-        if ($hw % 2 == 0){
+        if($hw % 2 == 0){
           echo "jenom lichá čísla pro šířku domku";
           return;
         }
-        if ($fh >= $hh){
+        if($fh >= $hh){
           echo "plot je většinou nižší než domek";
           return;
         }
-        if ($hh < 2 || $hw < 3){
+        if($hh < 2 || $hw < 3){
           echo "tam se nikdo nevejde";
           return;
         }
-        for ($i = 0; $i < ($hw / 2); $i++) {
+        for($i = 0; $i < ($hw / 2); $i++){
           echo str_repeat('&nbsp;', (($hw / 2) - $i));
           echo 'X';
           if($i != 0) echo str_repeat('&nbsp;', $i*2-1) . "X";
@@ -50,15 +50,15 @@
         ?>
         <br/>
         <?php
-        for ($i = 1; $i < $hh - 1; $i++) {
+        for($i = 1; $i < $hh - 1; $i++){
           echo 'X';
           echo $i % 2 == 1 ? 'o' . str_repeat("*o", ($hw - 3) / 2) : '*' . str_repeat("o*", ($hw - 3) / 2);
           echo 'X';
-          if ($hh - $i == $fh) {
-            print('-|-|-|');
+          if($hh - $i == $fh){
+            echo '-|-|-|';
           }
-          elseif ($hh - $i < $fh) {
-            print(' | | |');
+          elseif($hh - $i < $fh){
+            echo ' | | |';
           }
           echo '<br/>';
         }
