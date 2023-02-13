0d40adf83368a7b9e2b4ef0d1950171646191a2f
zvolaja <zvolaja@student.uzlabina.cz>
2023-02-13 19:15:57 +0100

    ASCII

---
+++ ASCII_art/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title></title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="./styles.css">
+  </head>
+  <body>
+    <div id="container">
+      <div id="in">
+        <div class="prompt">
+          <div class="txt">šířka domečku:</div>
+          <div><input type="number" name="num"></div>
+        </div> 
+        <div class="prompt">
+          <div class="txt">výška domečku:</div>
+          <div><input type="number" name="num"></div>
+        </div>
+        <div class="prompt">
+          <div class="txt">výška plotu:</div>
+          <div><input type="number" name="num"></div>
+        </div>
+      </div>
+      <div id="php">
+        <?php
+        
+        ?>
+      </div>
+    </div>
+  </body>
+</html>
